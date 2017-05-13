c34a6447eb57749e72c5bb1a96c5fd40f6d908fa
Eugene Low <low.eugene1@gmail.com>
2017-05-13 17:07:18 -0400

    re-added webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -22,15 +22,13 @@ module.exports = {
       }
     ]
   },
+  devtool: 'source-maps',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ]
 };
-
-
-// devtool: 'source-maps',
-// plugins: [
-//   new webpack.DefinePlugin({
-//     'process.env': {
-//       NODE_ENV: JSON.stringify('production')
-//     }
-//   }),
-//   new webpack.optimize.UglifyJsPlugin()
-// ]
